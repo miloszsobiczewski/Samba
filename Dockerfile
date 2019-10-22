33a67abe8403188cd9cd8cc5d5cf3d950f01a6b1
Milosz-mrx <m.sobiczewski@merixstudio.com>
2019-10-22 18:22:53 +0200

    complete docker setup

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM python:3-onbuild
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
 COPY . /usr/src/app
 EXPOSE 8000
--- Samba/settings.py
+++ Samba/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5u&6@$ijf0psb2yl(b0_p0^af0d2idx17cx)ks0$2v^8qka^%^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 
 # Application definition
