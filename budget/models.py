bca01b61531b85f05803cf3e1db59591fe433bed
Milosz-mrx <m.sobiczewski@merixstudio.com>
2019-10-27 13:25:37 +0100

    add icon field

--- budget/models.py
+++ budget/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    icon = models.CharField(max_length=10)
     sign = models.IntegerField()
 
     def __str__(self):
