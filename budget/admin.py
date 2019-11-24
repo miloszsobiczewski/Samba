a92990adca683cc3a72b7cfa4f855d0efb550435
Milosz-mrx <m.sobiczewski@merixstudio.com>
2019-11-24 16:11:04 +0100

    pagination, register admins

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Budget, Category
+
+
+@admin.register(Budget)
+class ActualisationAdmin(admin.ModelAdmin):
+    model = Budget
+    ordering = ["-date"]
+    list_display = ["date", "amount", "note", "category"]
+
+
+@admin.register(Category)
+class ActualisationAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ["name", "icon", "sign"]
